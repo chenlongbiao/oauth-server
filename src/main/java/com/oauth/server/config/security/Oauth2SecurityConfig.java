1afb6dfb2d9ff1a4cef6f9516e02d01d6713f2e8
chenlongbiao <123456>
2018-10-15 17:42:42 +0800

    基本sso实现

---
+++ src/main/java/com/oauth/server/config/security/Oauth2SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.oauth.server.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+
+/**
+ * @author chenlongbiao
+ * @version V1.0
+ * @Title:
+ * @date 2018/10/15
+ */
+@EnableWebSecurity
+public class Oauth2SecurityConfig extends WebSecurityConfigurerAdapter {
+    // 配置这个bean会在做AuthorizationServerConfigurer配置的时候使用
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("admin"))
+                .roles("test")
+        ;
+    }
+}
